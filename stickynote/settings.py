1701c4196da9f5ff3914b932dc35d3530ab2319c
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-23 20:49:33 +0100

    update

--- stickynote/settings.py
+++ stickynote/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3g%4^yhz&_ekh)@75i8vt=_yywlf#8gp!60_3d03u6ygwoa_9(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['sticky-note-api-crud.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
