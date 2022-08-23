30ba305dec42e816e32e99a8c1ddce5e075cf67c
Ayeni Olakunle <ayeniolakunle98@gmail.com>
2022-08-23 20:33:10 +0100

    Update

--- stickynote/settings.py
+++ stickynote/settings.py
@@ -127,5 +127,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 django_heroku.settings(locals())
